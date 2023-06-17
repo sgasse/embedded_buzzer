ee3db224147f4ecd4597e2b91ffb2fc667195d5f
Simon B. Gasse <sgasse@users.noreply.github.com>
2023-06-17 13:53:06 +0200

    Working blinky

--- buzzer_board/src/bin/dummy.rs
+++
@@ -1,9 +0,0 @@
-#![no_std]
-#![no_main]
-#![feature(type_alias_impl_trait)]
-
-use embassy_executor::Spawner;
-use panic_probe as _;
-
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {}
