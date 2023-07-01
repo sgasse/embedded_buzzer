039aad59dde5966b82a92b9ebf72fc6d39074040
Simon B. Gasse <sgasse@users.noreply.github.com>
2023-07-01 13:38:15 +0200

    Use generated JS in frontend

--- server/ts/index.ts
+++ server/ts/index.ts
@@ -1,1 +1,18 @@
-const THROTTLE: number = 12;
+
+var backend = new WebSocket("ws://127.0.0.1:3001/ws");
+
+const player = (msg: MessageEvent<any>) => {
+  var createSound: HTMLAudioElement = document.getElementById("create") as HTMLAudioElement;
+  console.log(msg);
+
+  if (createSound.paused) {
+    createSound.play();
+  } else {
+    createSound.currentTime = 0;
+  }
+};
+
+const init = () => {
+  backend.send("Init message from frontend");
+  backend.addEventListener("message", player);
+};
