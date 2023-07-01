037fe149796826899c80c831baade8bcdcf14787
Simon B. Gasse <sgasse@users.noreply.github.com>
2023-07-01 08:54:08 +0200

    Make `Message` formattable

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,21 +1,22 @@
 #![no_std]
 
+use defmt::Format;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Format)]
 pub struct GameInfo {
     pub instruction: u32,
     pub id: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Format)]
 pub enum Message {
     InitGame,
     Ping(u32),
     ButtonPress(ButtonPress),
 }
 
-#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Format)]
 pub struct ButtonPress {
     pub button_id: u8,
     pub millis_since_init: u32,
