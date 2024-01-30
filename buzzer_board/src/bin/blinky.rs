9df21bf71eafa94129cb7b7b67888db7f7700cdf
Simon B. Gasse <sgasse@users.noreply.github.com>
2024-01-30 20:55:03 +0100

    Tidy up blinky

--- buzzer_board/src/bin/blinky.rs
+++ buzzer_board/src/bin/blinky.rs
@@ -4,8 +4,8 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::Config;
 use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::Config;
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -19,23 +19,23 @@ async fn main(_spawner: Spawner) {
 
     let p = embassy_stm32::init(config);
 
-    info!("Hello World!");
+    info!("Let's blink some LEDs!");
 
     let mut led_green = Output::new(p.PJ2, Level::High, Speed::Low);
     let mut led_red = Output::new(p.PI13, Level::High, Speed::Low);
-    // let mut led_lcd = Output::new(p.PK0, Level::High, Speed::Low);
+    let mut led_lcd = Output::new(p.PK0, Level::High, Speed::Low);
 
     loop {
-        info!("high");
+        info!("High");
         led_green.set_high();
         led_red.set_high();
-        // led_lcd.set_high();
+        led_lcd.set_high();
         Timer::after(Duration::from_millis(200)).await;
 
-        info!("low");
+        info!("Low");
         led_green.set_low();
         led_red.set_low();
-        // led_lcd.set_low();
+        led_lcd.set_low();
         Timer::after(Duration::from_millis(800)).await;
     }
 }
