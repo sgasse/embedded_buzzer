ec3ff5f87581e6a06433c9541f3f3007d0fb98bf
Simon B. Gasse <sgasse@users.noreply.github.com>
2024-01-30 20:38:15 +0100

    Switch AoE2 create to button 0

--- server/ts/idMap.ts
+++ server/ts/idMap.ts
@@ -1,19 +1,19 @@
 export const ID_TO_SOUND = [
+    'create',
     'duck',
     'icq',
     'mario',
     'mgs',
     'partyblower',
     'wololo',
-    'create',
 ];
 
 export const ID_TO_NAME = [
+    'AoE2 Create',
     'Duck',
     'ICQ',
     'Mario',
     'MGS',
     'Partyblower',
     'Wololo',
-    'AoE2 Create',
 ];
